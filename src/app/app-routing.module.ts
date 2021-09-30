5b29c3a74ca1c58288aad7de6722ed240b68ca80
kotharishubh03 <shubh.kothari1111@gmail.com>
2021-10-01 01:14:35 +0530

    App Completed Signed-off-by: kotharishubh03 <shubh.kothari1111@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './myComponent/about/about.component';
+import { TodosComponent } from './myComponent/todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodosComponent },
+  { path: 'About', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './myComponent/todos/todos.component';
+import { TodoItemComponent } from './myComponent/todo-item/todo-item.component';
+import { AddtodoComponent } from './myComponent/addtodo/addtodo.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './myComponent/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    AddtodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
